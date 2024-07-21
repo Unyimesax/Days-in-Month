ef05ccd0677edc092a063fc4d8f9fcb854a5f2a3
UNYIME INOH <40715735+Unyimesax@users.noreply.github.com>
2024-07-21 17:25:55 +0000

    Added the solution code to complete the exercise.

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year")
+        return True
       else:
-        print("Not leap year")
+        return False
     else:
-      print("Leap year")
+      return True
   else:
-    print("Not leap year")
+    return False
 
 # TODO: Add more code here 👇
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+def days_in_month(year,month):
+  u_month = month -1
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year):
+    month_days[1] = 29
+  days = month_days[u_month] 
+  return days
 
 
 #🚨 Do NOT change any of the code below 
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
+year = int(input("Enter a year")) # 
+month = int(input("Enter a month")) # 
 days = days_in_month(year, month)
 print(days)
